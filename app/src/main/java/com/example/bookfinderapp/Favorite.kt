9c43e22d2801f60a05bb477c5bb40e2d727a3924
Bao Phuc Nguyen <156946937+baophucnguyen761@users.noreply.github.com>
2024-12-03 10:05:25 -0600

    Update and rename wishlist.kt to Favorite.kt

--- app/src/main/java/com/example/bookfinderapp/Favorite.kt
+++ app/src/main/java/com/example/bookfinderapp/Favorite.kt
@@ -8,7 +8,7 @@ import androidx.core.view.WindowInsetsCompat
 
 
 
-class wishlist : AppCompatActivity() {
+class Favorite : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
        super.onCreate(savedInstanceState)
         setContentView(R.layout.wishlist_book)
